c6d28c79bcf6eb649e6fc6b544fa9fae4de357af
poohvp <poohvp1147@gmail.com>
2023-10-09 01:32:56 +0700

    set path other

--- src/blog/components/Box.jsx
+++ src/blog/components/Box.jsx
@@ -5,7 +5,7 @@ function Box(props) {
     return (
       
       <div className="allblog duration-500 w-2/3 lg:w-[30%] md:w-[45%] rounded-md overflow-hidden mx-auto mt-8 h-auto bg-[#404040]">
-       <Link to="/blog"> <div className="earth w-full h-56 bg-center bg-cover invert"></div>
+       <Link to="/Blog-Platform/Blog"> <div className="earth w-full h-56 bg-center bg-cover invert"></div>
         <div className="text-white pt-4 text-3xl font-bold mx-4">Headline</div>
         <div className="text-white/[.7] pt-4 text-lg font-light mx-4 mb-2">Adipisicing culpa ut consectetur sint incididunt elit...</div>
         <hr className="opacity-20" />
