ac23212244d1e9c8b43dbf731b299fc144c4cead
poohvp <poohvp1147@gmail.com>
2023-10-15 17:32:43 +0700

    block animation

--- src/blog/components/Box.jsx
+++ src/blog/components/Box.jsx
@@ -6,7 +6,7 @@ function Box(props) {
       <div className="allblog duration-500 w-4/5 lg:w-[30%] md:w-[45%] rounded-md overflow-hidden mx-auto mt-8 h-auto dark:bg-[#404040] solid border-[1.25px] border-black/40 hover:border-black dark:border-transparent" data-aos-duration="500">
        <Link to="/Blog-Platform/Blog"> <div style={{ 
       backgroundImage: `url(${props.bgBlog})` 
-    }} className={`w-full h-56 bg-center bg-cover`}></div>
+    }} className={`w-full h-56 bg-left bg-cover bg-repeat duration-500 hover:bg-right`}></div>
     <div className="mx-4 pt-4"><button className="border-[1.5px] solid border-[#7971EA] dark:border-pink-400 px-2 py-1 text-xs text-[#7971EA] dark:text-pink-400">{props.category}</button></div>
         <div className={`text-black/70 dark:text-white text-3xl font-bold mx-4 ${props.Ref}`}>{props.heading}</div>
         <div className="text-black/60 dark:text-white/[.7] pt-4 text-lg font-light mx-4 mb-2">Adipisicing culpa ut consectetur sint incididunt elit...</div>
